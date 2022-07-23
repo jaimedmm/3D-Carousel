86b3e648413603a402e893020264e7696d485697
Jaime Daniel Martinez Moreno <jaimarti@microsoft.com>
2022-07-22 18:51:30 -0700

    import models

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+const express = require('express')
+var app = express();
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('index.html', function (error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
+app.use('/public', express.static('public'))
 
-        res.end()
-    })
-})
+app.set('view engine', 'ejs')
 
-server.listen(port, function (error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    } else {
-        console.log('Server is listening on port ' + port)
-    }
-})
+app.get('/', (req, res) => {
+    // res.sendStatus(200)
+    res.render('index')
+});
+
+app.listen(3000)
